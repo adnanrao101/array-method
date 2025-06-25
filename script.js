0e98eccbb7c007ad751253da65357080cf4fd992
Adnan Rao <adnanejaz2874@gmail.com>
2025-06-25 15:01:07 +0500

    today work

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 let fruits = ["apple", "banana", "mango"];
-document.write(fruits.length);
+// document.write(fruits.length);  array.length
+
+console.log(fruits.toString()); array.toString()
+
+console.log(fruits.at(0));  
+console.log(fruits.at(-1)); array.at(index)
