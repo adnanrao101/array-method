aef424b244ba5fef56db3d88a76159539beb17e9
Adnan Rao <adnanejaz2874@gmail.com>
2025-06-25 16:31:36 +0500

    3 more methods

--- script.js
+++ script.js
@@ -29,19 +29,39 @@
 
 // 1. array.length
 
-let fruits = ["apple", "banana", "mango", "grapes", "orange"];
-console.log(fruits.length); // 3
+// let fruits = ["apple", "banana", "mango", "grapes", "orange"];
+// console.log(fruits.length); // 3
 
 
 //  2. array.toString()
 
-let fruits = ["apple", "banana", "mango", "grapes", "orange"];
-
-console.log(fruits.toString())
+// let fruits = ["apple", "banana", "mango", "grapes", "orange"];
+// console.log(fruits.toString())
 
 
 // 3. array.at(index)
 
-let fruits = ["apple", "banana", "mango", "grapes", "orange"];
-console.log(fruits.at(0))
-console.log(fruits.at(-4))
+// let fruits = ["apple", "banana", "mango", "grapes", "orange"];
+// console.log(fruits.at(0))
+// console.log(fruits.at(-4))
+
+
+// 4. array.join(separator)
+
+// let fruits = ["apple", "banana", "mango", "grapes", "orange"];
+// console.log(fruits.join(" / "))
+
+
+//  5. array.pop()
+
+// let fruits = ["apple", "banana", "mango", "grapes", "orange"];
+// fruits.pop()
+// console.log(fruits)
+
+
+//  6. array.push(value)
+
+// let fruits = ["apple", "banana", "mango", "grapes", "orange"];
+// fruits.push("kiwi")
+// console.log(fruits)
+
